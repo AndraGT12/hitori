78265e73857b9098c6290fbfcc4e12a586c5611f
AndraGT12 <ahaiahdu@gmail.com>
2024-10-11 23:09:01 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188','6281356484612','6285773557984']
-global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.owner = ['6282155737388','6282155737388','6282155737388']
+global.packname = 'Bot AndraGT'
+global.author = 'AndraGT'
+global.botname = 'AndraGT Bot'
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json'
 global.pairing_code = true
@@ -26,9 +26,9 @@ global.fake = {
 }
 
 global.my = {
-	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/nazedev',
-	gc: 'https://chat.whatsapp.com/Hx9vcBVhbc04KLVGPFtH2R',
+	yt: '-',
+	gh: '-',
+	gc: 'https://chat.whatsapp.com/JXfXGaGXEYP2qosk9hHSIo',
 	ch: '120363250409960161@newsletter',
 }
 
